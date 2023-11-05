93ae946684ca85fad6373b43cb121f3330a068d4
sarthakgupta04 <gsarthak283@gmail.com>
2023-11-05 15:12:34 +0530

    hello

---
+++ app.js
@@ -0,0 +1,27 @@
+const toggleSwitch = document.querySelector(".toggle__input");
+
+// Check localStorage for saved theme
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  // Make sure toggle switch matches current theme
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
